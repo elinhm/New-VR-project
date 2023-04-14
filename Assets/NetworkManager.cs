8b52680573c06cb3ca6693f26e89064ecc845784
Amanda Lind Jonsson <amandizz_96@hotmail.com>
2023-04-14 10:54:27 +0200

    Photon-setup-14/4

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectToServer();
+    }
+
+    void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Try connect to server...");
+    }
+
+}
+
