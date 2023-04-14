df45f9e96319c8b98b9af49753647a5e7cb0fb4b
Amanda Lind Jonsson <amandizz_96@hotmail.com>
2023-04-14 11:40:39 +0200

    Senaste photon-setup

--- Assets/NetworkManager.cs
+++ Assets/NetworkManager.cs
@@ -17,6 +17,22 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         PhotonNetwork.ConnectUsingSettings();
         Debug.Log("Try connect to server...");
     }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to server.");
+        base.OnConnectedToMaster();
+        RoomOptions RoomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+
+        PhotonNetwork.JoinOrCreateRoom("Room1", roomOptions, TypedLobby.Default);
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a Room");
+        base.OnJoinedRoom();
+    }
 
 }
 
